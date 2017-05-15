16a99d18e1ecc931c875f81be4c340cee0b3d0bc
Spalexy <alex322p9@gmail.com>
2017-05-15 22:08:21 +0300

    Добавил строку меню, пункт 'exit'

--- editor.py
+++ editor.py
@@ -1,13 +1,23 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, qApp, QMainWindow, QAction
+from PyQt5.QtGui import QIcon
 
-
-class MyWidget(QWidget):
+class MyWidget(QMainWindow):
     def __init__(self):
-        QWidget.__init__(self)
+        QMainWindow.__init__(self)
         self.resize(250, 150)
         self.setWindowTitle('simple')
-        self.setWindowIcon(QIcon('icon/web.png'))
+        self.setWindowIcon(QIcon('pencil.jpg'))
+
+        exitAction = QAction(QIcon('exit.png'), '&Exit', self)
+        exitAction.setShortcut('Ctrl+Q')
+        exitAction.setStatusTip('Exit application')
+        exitAction.triggered.connect(qApp.quit)
+
+        self.statusBar()
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+        fileMenu.addAction(exitAction)
         self.show()
 
 
